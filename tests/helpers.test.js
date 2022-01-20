dc90bcd111f2b44924977614b8fb625052f06704
Bryce Miller <bryce@brycem.co>
2022-01-19 21:52:05 -0500

    start tests

---
+++ tests/helpers.test.js
@@ -0,0 +1,10 @@
+//import { httpResponse } from '../src/helpers.js'
+
+//test('HTTP Responses formatted Correctly', () => {
+//const result = httpResponse(200, 'Test Message')
+
+//expect(result).toEqual({
+//statusCode: 200,
+//body: '{"message":"Test Message"}',
+//})
+//})
