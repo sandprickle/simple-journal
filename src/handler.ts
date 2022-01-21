473032bb63407bf73e4644874243d8d83a87f0a4
Bryce Miller <bryce@brycem.co>
2022-01-20 19:16:56 -0500

    cleanup

--- src/handler.ts
+++ src/handler.ts
@@ -2,13 +2,11 @@ import { httpResponse, validateEntry } from './helpers'
 import { storeEntry } from './db'
 
 const createEntry = async function createNewJournalEntry(event: any) {
-  try {
-    const entry = await validateEntry(event.body)
-
-    const res = await storeEntry(entry)
-    console.log(res)
-  } catch (err) {
-    console.error(err)
-  }
-  return httpResponse(200, 'It seems to be working.')
+  return validateEntry(event.body)
+    .then((entry) => storeEntry(entry))
+    .catch((err) => {
+      if (err.type && err.type === 'ValidationError')
+        return httpResponse(400, JSON.stringify(err.errors))
+      else return httpResponse(500, JSON.stringify(err))
+    })
 }
