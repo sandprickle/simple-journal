94ec808b08dc0d53d17469d6e5f9871059bf63d6
Bryce Miller <bryce@brycem.co>
2022-01-27 18:01:56 -0500

    disable get endpoint for now

--- src/handler.ts
+++ src/handler.ts
@@ -19,9 +19,6 @@ const httpResponse = (code: number, message: string): HttpResponse => {
 }
 
 const validateEntry = (entry: JournalEntry): Promise<JournalEntry> => {
-  //const isoDateExp =
-  ///^([+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24:?00)([.,]\d+(?!:))?)?(\17[0-5]\d([.,]\d+)?)?([zZ]|([+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/
-
   const journalEntrySchema = yup
     .object({
       journalId: yup.string().defined().min(3),
@@ -33,9 +30,9 @@ const validateEntry = (entry: JournalEntry): Promise<JournalEntry> => {
   return journalEntrySchema.validate(entry, { strict: true })
 }
 
-const validateJournalId = (journalId: string) => {
-  return yup.string().min(3).defined().validate(journalId)
-}
+//const validateJournalId = (journalId: string) => {
+//return yup.string().min(3).defined().validate(journalId)
+//}
 
 // HANDLERS
 
@@ -53,16 +50,16 @@ export const createEntry = async (event: any) => {
     })
 }
 
-export const getJournalEntries = async (event: any) => {
-  console.log(event)
+//export const getJournalEntries = async (event: any) => {
+//console.log(event)
 
-  return validateJournalId(event.queryStringParameters.journal)
-    .then((journalId) => getEntries(journalId))
-    .catch((err) => {
-      console.error(err)
+//return validateJournalId(event.queryStringParameters.journal)
+//.then((journalId) => getEntries(journalId))
+//.catch((err) => {
+//console.error(err)
 
-      if (err.name && err.name === 'ValidationError')
-        return httpResponse(400, JSON.stringify(err.errors))
-      else return httpResponse(500, 'Yo womething wonky happened')
-    })
-}
+//if (err.name && err.name === 'ValidationError')
+//return httpResponse(400, JSON.stringify(err.errors))
+//else return httpResponse(500, 'Yo womething wonky happened')
+//})
+//}
